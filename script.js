ce92d10a2d9b5cab1be6d6598d974648ad49ea60
tastas000 <TiiLg0o000@gmail.com>
2023-10-18 22:40:17 +0700

    Add password matching script

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+const password = document.querySelector('#password');
+const confirmPassword = document.querySelector('#confirm-password');
+const passwordWarning = document.querySelector('#password + p');
+
+password.classList.add('password-check');
+confirmPassword.classList.add('password-check');
+
+
+password.addEventListener('input', passwordCheck);
+confirmPassword.addEventListener('input', passwordCheck);
+
+function passwordCheck(){
+  if (password.value === confirmPassword.value
+    && password.value !== ''
+    && confirmPassword.value !== ''){
+    password.classList.remove('password-check');
+    confirmPassword.classList.remove('password-check');
+    passwordWarning.style.display = 'none';
+  }
+  else {
+    password.classList.add('password-check');
+    confirmPassword.classList.add('password-check');
+    passwordWarning.style.display = 'block';
+  }
+}
 
